c879f741bcf853934ecba3ee3d43ead4fbffb03e
arevaloprincess <163614830+arevaloprincess@users.noreply.github.com>
2024-07-12 20:50:33 +0800

    Add files via upload

---
+++ CessPortfolio/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    path("", views.home, name = "home"),
+    path("about/", views.about, name = "about"),
+    path("portfolio/", views.portfolio, name = "portfolio"),
+    path("contacts/", views.contacts, name = "contacts")
+]
